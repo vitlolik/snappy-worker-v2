e556eb91f2b6f6f7e6de07f5b2c5728368c2d6fb
Vitaliy Berekchiyan <v.berekchiyan@semrush.com>
2023-11-06 21:05:39 +0100

    fixed nginx, and api prefix

--- src/filters/telegraf-exception.filter.ts
+++ src/filters/telegraf-exception.filter.ts
@@ -2,7 +2,7 @@ import { Catch, ExceptionFilter } from '@nestjs/common';
 import { TelegrafException } from 'nestjs-telegraf';
 import { TelegramError } from 'telegraf';
 
-@Catch(TelegrafException, TelegramError, Error)
+@Catch(TelegrafException, TelegramError)
 export class TelegrafExceptionFilter implements ExceptionFilter {
   async catch(exception: TelegrafException | TelegramError): Promise<void> {
     console.log('TELEGRAM BOT ERROR', exception);
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { TelegrafExceptionFilter } from './filters/telegraf-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
   app.useGlobalFilters(new TelegrafExceptionFilter());
+
   await app.listen(process.env.APP_PORT || 3000);
 }
+
 bootstrap();
